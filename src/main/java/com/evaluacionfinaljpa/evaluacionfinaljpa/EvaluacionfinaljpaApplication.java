ebe5708b2b91b1ebd57b7d76ce4e7faec232e9ac
ronaldomtlp-125 <ronaldomtlp125@gmail.com>
2024-05-06 08:08:26 -0500

    Creación del proyecto con SpringInitializr

---
+++ src/main/java/com/evaluacionfinaljpa/evaluacionfinaljpa/EvaluacionfinaljpaApplication.java
@@ -0,0 +1,13 @@
+package com.evaluacionfinaljpa.evaluacionfinaljpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluacionfinaljpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluacionfinaljpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evaluacionfinaljpa/evaluacionfinaljpa/EvaluacionfinaljpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evaluacionfinaljpa.evaluacionfinaljpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluacionfinaljpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
